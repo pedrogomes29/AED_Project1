f9b26c1888c1511cb7a754cffeda496909c15679
Pedro Gomes <pedromgomes29@gmail.com>
2021-12-01 16:03:33 +0000

    Added class Service

---
+++ air_line/Service.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pedro on 01/12/21.
+//
+
+#include "Service.h"
---
+++ air_line/Service.h
@@ -0,0 +1,14 @@
+//
+// Created by pedro on 01/12/21.
+//
+
+#ifndef AIRLINE_SERVICE_H
+#define AIRLINE_SERVICE_H
+
+
+class Service {
+
+};
+
+
+#endif //AIRLINE_SERVICE_H
