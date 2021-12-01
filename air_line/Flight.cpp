0e91ba6058202eaebdba75491ea25fc779db4dc0
ruiLaptop <mrswager173@gmail.com>
2021-12-01 19:11:05 +0000

    Added getters for class Flight

--- air_line/Flight.cpp
+++ air_line/Flight.cpp
@@ -4,7 +4,7 @@
 
 #include "Flight.h"
 
-Flight::Flight(unsigned capacity,unsigned number,double duration,const string& date_of_departure, unsigned hour,unsigned minute, string origin,
+Flight::Flight(unsigned capacity,unsigned number,Time duration,const string& date_of_departure, unsigned hour,unsigned minute, string origin,
                string destination, vector<Passenger> passengers){
 this->capacity = capacity;
 this->number = number;
@@ -25,4 +25,24 @@ bool Flight::add_passenger(Passenger p) {
         return true;
     }
     return false;
+}
+
+unsigned Flight::get_number() const {
+    return number;
+}
+
+Time Flight::get_duration() const {
+    return duration;
+}
+
+string Flight::get_origin() const{
+    return origin;
+}
+
+string Flight::get_destination() const {
+    return destination;
+}
+
+vector<Passenger> Flight::get_passengers() const {
+    return passengers;
 }
