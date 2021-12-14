37bdbbe5dee65f562d7b83246b99fb43a9080503
Pedro Gomes <pedromgomes29@gmail.com>
2021-12-14 20:47:02 +0000

    Airport

---
+++ air_line/Airport.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pedro on 14/12/2021.
+//
+
+#include "Airport.h"
---
+++ air_line/Airport.h
@@ -0,0 +1,21 @@
+//
+// Created by Pedro on 14/12/2021.
+//
+
+#ifndef AIRLINE_AIRPORT_H
+#define AIRLINE_AIRPORT_H
+#include <string>
+
+using namespace std;
+
+class Airport {
+
+private:
+    string name;
+    BST<LocalTransport> transports;
+public:
+    Airport(string name,)
+};
+
+
+#endif //AIRLINE_AIRPORT_H
