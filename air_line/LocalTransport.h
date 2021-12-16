1b39a46c165c1451be64039a898da954a43f5ceb
rui-exe <mrswager173@gmail.com>
2021-12-16 12:18:59 +0000

    Commented a few .h files

--- air_line/LocalTransport.h
+++ air_line/LocalTransport.h
@@ -18,18 +18,78 @@ private:
     string type;
     vector<Time> schedules;
 public:
+    /**
+     * Default constructor of LocalTransport class
+     * Default local Transport is a local transport
+     * of name "" distance = 0 and type = ""
+     * with no schedules
+     */
     LocalTransport();
+    /**
+     * Constructor of class LocalTransport
+     * @param name string representing the name of the LocalTransport
+     * @param distance float indicating the distance of an airport to this localTransport
+     * @param type string representing either train a subway or a bus.
+     * @param schedules ordered vector of schedules that store the schedules of this particular LocalTransport
+     */
     LocalTransport(string name,float distance,string type,vector<Time> schedules);
+    /**
+     * getter for the name attribute
+     * @return returns a string representing the name of the LocalTransport
+     */
     string get_name() const;
+    /**
+     * setter for the name of the LocalTransport
+     * @param name string representing the name of the LocalTransport
+     */
     void set_name(string name);
+    /**
+     * getter for the distance attribute
+     * @return returns float indicating the distance of an airport to this localTransport
+     */
     float get_distance() const;
+    /**
+     * setter for the distance attribute
+     * @param d float indicating the distance of an airport to this localTransport
+     */
     void set_distance(float &d);
+    /**
+     * getter for the type attribute
+     * @return returns a string representing the type of the LocalTransport
+     */
     string get_type() const;
+    /**
+     * setter for the type attribute
+     * @param t string representing the type of the LocalTransport
+     */
     void set_type(string &t);
+    /**
+     * getter for all the schedules of this LocalTransport
+     * @return returns ordered vector of schedules that store the schedules of this particular LocalTransport
+     */
     vector<Time> get_schedules() const;
+    /**
+     * setter for all the schedules of this LocalTransport
+     * @param s ordered vector of schedules that store the schedules of this particular LocalTransport
+     */
     void set_schedules(vector<Time> &s);
+    /**
+     * Adds an object of class Schedule t to the ordered vector schedules in the correct
+     * @param s
+     */
     void add_schedule(Time s);
+    /**
+     * Overloading of the < operator used to compare two LocalTransport.
+     * @param lt1 object of class LocalTransport passed by reference.
+     * @return returns a boolean value indicating whether this local transport is lesser than lt1. We compare them by distance to the airport
+     */
     bool operator<(const LocalTransport &lt1) const;
+    /**
+     *
+     * @param n integer representing the number of schedules the user wishes to see
+     * @param t object of class time that represents the lower time limit for the desired schedules output that the user wants to see.
+     * @return returns ordered vector containing n schedules after the time t given by by the user.
+     */
     vector<Time> next_schedules(int n, Time t);
 };
 
--- air_line/Time.h
+++ air_line/Time.h
@@ -11,15 +11,62 @@ private:
     int hour;
     int minute;
 public:
+    /**
+     * Default constructor of Time class
+     * Default hour and minute is midnight
+     */
     Time();
+    /**
+     * Constructor of Time class
+     * @param hour integer representing an hour of a day
+     * @param minute int representing the minutes of an hour
+     */
     Time(int hour,int minute);
+    /**
+     * setter for the hour attribute
+     * @param hour integer representing an hour of a day
+     */
     void set_hour(int hour);
+    /**
+     * setter for the minute attribute
+     * @param minute integer representing the minutes of an hour
+     */
     void set_minute (int minute);
+    /**
+     * getter for hour attribute
+     * @return integer representing an hour of a day
+     */
     int get_hour() const;
+    /**
+     * getter for minute attribute
+     * @return int representing the minutes of an hour
+     */
     int get_minute() const;
+    /**
+     * Overloading of the < operator used to compare two Times.
+     * @param time2 Object passed by reference of the same class.
+     * @return returns a boolean value indicating whether this time is lesser than time2
+     */
     bool operator<(Time const &time2) const;
+    /**
+     * Overloading of the >> input operator
+     * @param stream istream which will be modified and then used to read from a text file
+     * @param t object of Class Time passed by reference
+     * @return returns an istream which is ready to be used on to read from a text file
+     */
     friend istream &operator>>(istream& stream,  Time& t);
+    /**
+     * Overloading of the << output operator
+     * @param stream ostream which will be modified and then used to write on a text file
+     * @param t object of Class Time passed by reference
+     * @return returns an ostream which is ready to be used on to write on a text file
+     */
     friend ostream &operator<< (ostream& stream, const Time& t);
+    /**
+     * Overloading of the + operator so it can be made possible adding two hours
+     * @param t2 object of class time passed by reference.
+     * @return returns an object of class Time containing the result of the adding operation between two times.
+     */
     Time operator+(const Time &t2) const;
 };
 
